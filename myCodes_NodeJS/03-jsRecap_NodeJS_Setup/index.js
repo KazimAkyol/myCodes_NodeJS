dcf2446ef2c6d705acb73fa38374032ea10bf882
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-17 16:10:25 +0100

    add oop

--- myCodes_NodeJS/03-jsRecap_NodeJS_Setup/index.js
+++ myCodes_NodeJS/03-jsRecap_NodeJS_Setup/index.js
@@ -1,4 +1,31 @@
-'use strict'
+"use strict";
 // $ node
-// console.log('terminal');
+// console.log("terminal");
 
+// npm install --global nodemon
+// nodemon --version
+
+console.log("Nodemon ile js calistiriyoruz");
+
+let num1 = 5;
+console.log(typeof num1);
+let num2 = 14;
+let toplam = num1 + num2;
+console.log(toplam);
+console.log(`${num1 + num2}`);
+
+console.log("for döngüsü");
+
+for (let i = 1; i < 5; i++) {
+  console.log(`Sayi:${i}`);
+}
+
+function carpma(a, b) {
+  return a * b;
+}
+console.log(`${carpma(2, 10)}`);
+console.log(typeof `${carpma(2, 10)}`);
+
+const result = carpma(2, 8);
+console.log(result);
+console.log(typeof result);
---
+++ myCodes_NodeJS/04-OOP_1/oop1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* ----------------------------------------------------- * // alt + shift + a
+                        OBJECTS
+/* ----------------------------------------------------- */
+
+// Direkt objeleri isimlendirirken PascalCase veya cameLCase kullanabiliriz.
+const exampleObject = {
+  propertyName: "value", // attribute, field
+  methodName: function () {
+    return "methodName()is a method";
+  },
+
+  methodAlternative() {
+    return "methodAlternative() is a method";
+  },
+};
+
+console.log(exampleObject.propertyName);
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
+/* ----------------------------------------------------- */
