9ff90b34d75866a65da80d3c6114f93f025ff1a1
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-18 23:53:24 +0100

    add node-modules

--- myCodes_NodeJS/06-NodeJS_Modules/index.js
+++ myCodes_NodeJS/06-NodeJS_Modules/index.js
@@ -1,6 +1,16 @@
-"use strict"
+"use strict";
 
 // $ npm init -y
 // npm install // package-lock.json olusturur.
 // nodemon // nodemon index.js'de calistirir ve izler.
 
+console.log("indexjs");
+
+//? baska bir js dosyasini iceri aktarma:
+
+require("./module/module.js");
+// require("./module/module");
+
+//! tek fonksiyonu import etme
+const testFunc = require("./module/module.js");
+testFunc();
---
+++ myCodes_NodeJS/06-NodeJS_Modules/module/module.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("modul dosyasi calisti");
+
+//! tek fonksiyonu disari aktarma
+const testFunc = function () {
+  console.log("this is a function");
+};
+
+module.exports = testFunc;
