c5c67aa23c819abeaff7b28202b74490f0204834
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-19 12:19:47 +0100

    update server

---
+++ myCodes_NodeJS/07-NodeJS_Server/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// $ npm install dotenv
+
+require("dotenv").config(); //? .env dosyasindaki verileri process.env icine yükler.
+
+const PORT = process?.env.PORT || 8000;
+const HOST = process?.env.HOST || "127.0.0.1";
+
+const { error } = require("node:console");
+const http = require("node:http"); //? http modülünü cagirir. Bu node.js'in icindedir.
+
+const app = http.createServer((req, res) => {
+  //? Piyasa standarti app olarak isimlendirilir.
+  //   console.log(req); //? gelen HTTP istegini tüm detaylariyla yazdirir.
+  console.log("----------------------------");
+
+  if (req.url == "/") {
+    res.end("<h1>welcome</h1>");
+  } else if ((req.url = "/blogs")) {
+    res.end("<h1>Listelenen Bloglar</h1>");
+  } else if ((req.url = "/newBlog")) {
+    res.end("<h1>Yeni blog ekle</h1>");
+  }
+});
