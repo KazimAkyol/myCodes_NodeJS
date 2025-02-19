0d87035a6c086624337d28c1c4ff25f313b6d3e6
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-19 09:34:53 +0100

    update node-modules

--- myCodes_NodeJS/06-NodeJS_Modules/index.js
+++ myCodes_NodeJS/06-NodeJS_Modules/index.js
@@ -6,11 +6,35 @@
 
 console.log("indexjs");
 
+/* ----------------------------------------------------------------------------- */
+
 //? baska bir js dosyasini iceri aktarma:
 
 require("./module/module.js");
 // require("./module/module");
 
 //! tek fonksiyonu import etme
-const testFunc = require("./module/module.js");
-testFunc();
+// const testFunc = require("./module/module.js");
+// testFunc();
+
+//! ismi degistirilmis fonksiyonlari yeni isimleriyle destructuring ettik:
+// const { test1: func1, test2 } = require("./module/module.js");
+// func1();
+// test2();
+
+/* ----------------------------------------------------------------------------- */
+
+const singleFunc = require("./module/module.js");
+singleFunc();
+const { testFunc1, testFunc2 } = singleFunc();
+testFunc1();
+testFunc2();
+
+/* ----------------------------------------------------------------------------- *
+
+//! array olarak karsilama
+const [testFunc1, testFunc2] = require("./module/module.js");
+testFunc1();
+/* ----------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------- */
--- myCodes_NodeJS/06-NodeJS_Modules/module/module.js
+++ myCodes_NodeJS/06-NodeJS_Modules/module/module.js
@@ -3,8 +3,51 @@
 console.log("modul dosyasi calisti");
 
 //! tek fonksiyonu disari aktarma
-const testFunc = function () {
-  console.log("this is a function");
+// const testFunc = function () {
+//   console.log("this is a function");
+// };
+
+// module.exports = testFunc;
+
+/* ----------------------------------------------------------------------------- *
+
+//! birden fazla deger ve fonksiyon gönderme
+
+const yil = 2025;
+
+const testFunc1 = function () {
+  console.log("testFunc1 running");
+};
+const testFunc2 = function () {
+  console.log("testFunc2 running");
+};
+module.exports = {
+  //* obje icinde isim degistirme
+  test1: testFunc1,
+  test2: testFunc2,
 };
 
-module.exports = testFunc;
+/* ----------------------------------------------------------------------------- */
+
+//! tek fonksiyon icinden cagirma:
+
+const singleFunc = function () {
+  const yil = 2025;
+  console.log(yil);
+
+  const testFunc1 = function () {
+    console.log("testFunc1 running");
+  };
+
+  const testFunc2 = function () {
+    console.log("testFunc2 running");
+  };
+
+  return { testFunc1, testFunc2 };
+};
+
+module.exports = singleFunc;
+
+/* ----------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------- */
