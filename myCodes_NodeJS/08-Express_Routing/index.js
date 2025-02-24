8b7a5425e80a95e02983b0fd0c84f471df42e6aa
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-24 16:48:01 +0100

    add sequelize-2

--- myCodes_NodeJS/08-Express_Routing/index.js
+++ myCodes_NodeJS/08-Express_Routing/index.js
@@ -13,10 +13,26 @@ const express = require("express"); // Express değişkenine express Framework a
 
 const app = express(); // Run application on express.
 
-app.listen(8000, () => console.log("Running at: http://127.0.0.1:8000"));
+//* Env
+require("dotenv").config();
+const PORT = process.env.PORT;
 
 /* ------------------------------------------------------------------------ */
+
+//* HTTP_METHODS & URLS:
+
+app.get("/", (req, res) => res.end("called in 'get' method. "));
+app.post("/", (req, res) => res.end("called in 'post' method."));
+app.put("/", (req, res) => res.end("called in 'put' method."));
+app.patch("/", (req, res) => res.end("called in 'patch' method."));
+app.delete("/", (req, res) => res.end("called in 'delete' method."));
+
+//* Tüm methodlara izin ver;
+
 /* ------------------------------------------------------------------------ */
+
+app.listen(PORT, () => console.log("Running at: http://127.0.0.1:8000" + PORT));
+
 /* ------------------------------------------------------------------------ */
 /* ------------------------------------------------------------------------ */
 /* ------------------------------------------------------------------------ */
