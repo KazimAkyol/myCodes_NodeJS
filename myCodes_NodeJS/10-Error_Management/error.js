818a17a5bf56ee093895a8239f7d8d2f0792816e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-25 20:00:33 +0100

    add try-catch

--- myCodes_NodeJS/10-Error_Management/error.js
+++ myCodes_NodeJS/10-Error_Management/error.js
@@ -15,7 +15,7 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/*------------------------------------------------------ */
+/*------------------------------------------------------ *
 //* throw
 
 //* 1- json veri göndermemiz lazim
@@ -25,7 +25,7 @@ app.get("/user/:id", (req, res) => {
   const id = req.params.id;
   if (isNaN(id)) {
     res.errorStatusCode = 400;
-    throw new Error("ID is not a number", { cause: "paramas.id" + id });
+    throw new Error("ID is not a number", { cause: "params.id=" + id });
   } else {
     res.send({
       error: false,
@@ -34,10 +34,50 @@ app.get("/user/:id", (req, res) => {
   }
 });
 
+/*------------------------------------------------------ *
+//* try-catch
+
+app.get("/user/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    if (isNaN(id)) {
+      throw new Error("ID is not a number");
+    } else {
+      res.send({
+        error: false,
+        id,
+      });
+    }
+  } catch (error) {
+    // console.log(error);
+    // res.send({
+    //   error: true,
+    //   message: error.message,
+    // });
+    //* to send this error to errorHandler middleware
+    next(err); // if you send error in the next method, it goes directly to the error handler middleware function.
+  }
+});
+
+//* middleware fonksiyonunun calismasi icin try-catch blogunda next(err) yazarak errorHandler'a yönlendirme yapilarak(middleware fonksiyonunun calisma sistemi) Terminal'de console'a yazilan yazi görülür. Burada 'error handler worked' yazdirildi.
+
+/*------------------------------------------------------ *
+//* async-functions
+
+const asyncFn = async () => {
+  throw new Error("Created error in async-fn");
+};
+
+app.get("/async", async (req, resizeBy, next) => {
+  await asyncFn().catch(next);
+});
+
 /*------------------------------------------------------ */
-/*------------------------------------------------------ */
-/*------------------------------------------------------ */
+//* express-async-errors
+
+//* 
 /*------------------------------------------------------ */
 /*------------------------------------------------------ */
 
-app.listen(PORT, () => console.log("Running http://127.0.0.1: + PORT"));
+app.listen(PORT, () => console.log("Running http://127.0.0.1:" + PORT));
