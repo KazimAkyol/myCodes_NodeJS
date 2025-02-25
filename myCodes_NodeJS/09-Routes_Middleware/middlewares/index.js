8bdd3846d10afbf6ce2dfb037c2c7606df41286b
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-25 17:32:06 +0100

    add error-management

--- myCodes_NodeJS/09-Routes_Middleware/middlewares/index.js
+++ myCodes_NodeJS/09-Routes_Middleware/middlewares/index.js
@@ -2,4 +2,41 @@
 
 /* --------------------------------------------------------- *
                    EXPRESSJS - MIDDLEWARES
+/* --------------------------------------------------------- *
+
+const middleFn1 = (req, res, next) => {
+  console.log("middleware function 1 started");
+  next();
+};
+
+const middleFn2 = (req, res, next) => {
+  console.log("middleware function 2 started");
+  next();
+};
+
+module.exports = { middleFn1, middleFn2 }; //* obje icinde birden fazla function export edilebilir.
+
+/* --------------------------------------------------------- *
+
+module.exports.middleFn1 = (req, res, next) => {
+  console.log("middleware function 1 started");
+  next();
+};
+
+module.exports.middleFn2 = (req, res, next) => {
+  console.log("middleware function 2 started");
+  next();
+};
+
 /* --------------------------------------------------------- */
+
+module.exports = {
+  middleFn1: (req, res, next) => {
+    console.log("middleware function 1 started");
+    next();
+  },
+  middleFn2: (req, res, next) => {
+    console.log("middleware function 2 started");
+    next();
+  },
+};
---
+++ myCodes_NodeJS/10-Error_Management/error.js
@@ -0,0 +1,10 @@
+"use strict";
+/* -------------------------------------------------------
+                EXPRESSJS - ERROR MANAGEMENT
+------------------------------------------------------- */
+
+// $ npm init -y => create package.json
+// $ npm i express dotenv
+// $ echo PORT=8000 > .env
+// cat > .gitignore && paste gitignore content & ctrl+c
+// $ nodemon
