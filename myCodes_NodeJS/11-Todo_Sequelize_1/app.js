ece496435d89a01506e770e5e8666c625b03bf87
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-02-27 20:33:22 +0100

    updaate sequelize

--- myCodes_NodeJS/11-Todo_Sequelize_1/app.js
+++ myCodes_NodeJS/11-Todo_Sequelize_1/app.js
@@ -17,8 +17,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-//* Accept json data
+// Accept json data
 app.use(express.json());
+
+// Catch async-errors:
+require("express-async-errors");
 
 app.all("/", (req, res) => {
   res.send("WELCOME TO TODO API");
@@ -26,8 +29,107 @@ app.all("/", (req, res) => {
 
 /* ------------------------------------------------------- */
 //* Sequelize
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+//* Where is DB(DB Connection Detail)
+const sequelize = new Sequelize("sqlite:./db.sqlite3");
+
+//* MODEL:
+//* sequelize.define('tableName', {columns})
+const Todo = sequelize.define("todos", {
+  //   id: { //* No need to define ID field, it will be created auto.
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false, // default:true
+  //     unique: true,
+  //     field: "custom_name",
+  //     comment: "description or comment",
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //   },
+
+  title: {
+    type: DataTypes.STRING(256), // varchar(256)
+    allowNull: false,
+  },
+
+  description: DataTypes.TEXT, // Shorthand using
+
+  priority: {
+    // -1: Low, 0: Normal, 1: High
+    type: DataTypes.SMALLINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
+  },
+
+  newField: DataTypes.TEXT,
+
+  // No need define createdAt & updateAt fields. They are default created.
+});
+
+//* Synchorization: // This methods should run only once.
+// sequelize.sync(); // Create table
+// sequelize.sync({ force: true }); // Drop Table & Create Table
+// sequelize.sync({ alter: true }); // Drop Table & Create Table
+
+//* DB Connection:
+sequelize
+  .authenticate()
+  .then(() => console.log("* DB Connected *"))
+  .catch(() => console.log("*! DB Not Connected ! *"));
+
 /* ------------------------------------------------------- */
+//* Routers
+const router = express.Router();
+
+// List Todos:
+router.get("/todo", async (req, res) => {
+  const result = await Todo.findAll(); // Select * From todos
+
+  res.send({
+    error: false,
+    result,
+  });
+});
+
+// Create Todo:
+router.post("/todo", async (req, res) => {
+//   const result = await Todo.create({
+//     title: "title-1",
+//     description: "description-1",
+//     isDone: false,
+//     priority: 0,
+//   });
+
+  res.send({
+    error: false,
+    result,
+  });
+});
+
+app.use(router);
+
 /* ------------------------------------------------------- */
+
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler worked.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    stack: err.stack, // error details
+  });
+};
+
+app.use(errorHandler);
+
 /* ------------------------------------------------------- */
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
