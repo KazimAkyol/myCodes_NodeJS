a79d0ce943affc33e072196312b5843fe21cb8c8
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-01 15:16:27 +0100

    update blogModel

--- myCodes_NodeJS/14-BlogApi_Mongoose/src/models/blogModel.js
+++ myCodes_NodeJS/14-BlogApi_Mongoose/src/models/blogModel.js
@@ -7,3 +7,61 @@
 
 const mongoose = require("mongoose");
 
+/* ----------------------------------------------------------------------- */
+//* BlogCtegory Schema
+
+// Create Schema
+const blogCategorySchema = new mongoose.Schema(
+  {
+    //_id: ,
+    name: {
+      type: String, // varchar
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    collection: "BlogCategories",
+  }
+);
+
+// Set Model
+const BlogCategory = mongoose.model("BlogCategory", blogCategorySchema);
+
+/* ----------------------------------------------------------------------- */
+//* BlogPost Schema
+
+// Create Schema
+
+const blogPostSchema = new mongoose.Schema(
+  {
+    // _id: ,
+
+    categoryId: {
+      // default relation: ManyToOne
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BlogCategory",
+      required: true,
+      unique: true, // convert to OneToOne Relation.
+    },
+
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    // createdAt,
+    // updatedAt
+  },
+  {
+    collection: "blogPosts",
+    timestamps: true,
+  }
+);
