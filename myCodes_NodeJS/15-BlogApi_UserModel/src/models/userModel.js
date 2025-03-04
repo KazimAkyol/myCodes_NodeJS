c4c515214c26c0f9e3043e4b118c0c425b50a52a
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-04 16:00:07 +0100

    add Password Encryption

--- myCodes_NodeJS/15-BlogApi_UserModel/src/models/userModel.js
+++ myCodes_NodeJS/15-BlogApi_UserModel/src/models/userModel.js
@@ -5,6 +5,23 @@
 
 const mongoose = require("mongoose");
 
+//* Password Encryption:
+// https://nodejs.org/docs/latest/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+
+const crypto = require("node:crypto");
+
+const salt = "asdfasdfg283rsadfkasdfasiurwasfsa";
+const iterations = 10000;
+const keylen = 32; // write 32 for 64
+const digest = "sha512";
+
+const passwordEncrypte = function (password) {
+  // pddf2Sync returns buffer string.
+  return crypto
+    .pbkdf2Sync(password, salt, iterations, keylen, digest)
+    .toString("hex");
+};
+
 /* -------------------------------------------------------*/
 
 const userSchema = new mongoose.Schema(
@@ -17,12 +34,40 @@ const userSchema = new mongoose.Schema(
       required: true,
       //   unique: true,
       unique: [true, "This email address is already in use."],
+      //* How validate works?
+      //   validate: () => {return false; }, // if validate return false it throws validation error.
+
+      //   //* Throw validation error
+      //   validate: (email) => {
+      //     // console.log(email.includes("@") && email.includes("."));
+      //     if (email.includes("@") && email.includes(".")) {
+      //       return true;
+      //     }
+      //     return false;
+      //   },
+
+      //* Throw validation error with custom message
+      validate: [
+        (email) => {
+          // console.log(email.includes("@") && email.includes("."));
+          if (email.includes("@") && email.includes(".")) {
+            return true;
+          }
+          return false;
+        },
+        "Please enter a valid email address.",
+      ],
     },
 
     password: {
       type: String,
       trim: true,
       required: true,
+      //* How set method works?
+      //   set: (password) => { return "clarusway" }, // whatever value set method returns it will be save to DB.
+      set: (password) => {
+        return password + "edited";
+      },
     },
 
     firstName: String,
@@ -34,4 +79,4 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
--- myCodes_NodeJS/15-BlogApi_UserModel/src/routes/userRouter.js
+++ myCodes_NodeJS/15-BlogApi_UserModel/src/routes/userRouter.js
@@ -7,13 +7,13 @@ const router = require("express").Router();
 
 const user = require("../controllers/userController");
 
-/* ------------------------------------------------------- */
+/* ----------------------------------------------------- */
 // URL: /user ->
 
 router.route("/").get(user.list).post(user.create);
 
 router.route("/:userId").get(user.read).put(user.update).delete(user.delete);
 
-/* ------------------------------------------------------- */
+/* ----------------------------------------------------- */
 
 module.exports = router;
