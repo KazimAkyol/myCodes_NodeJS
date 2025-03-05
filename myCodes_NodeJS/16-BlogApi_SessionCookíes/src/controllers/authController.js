3d0da88467d24bd62aef5653fbea6d9aa7059c00
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-05 13:02:02 +0100

    add npm i cookie-session

--- myCodes_NodeJS/16-BlogApi_SessionCookíes/src/controllers/authController.js
+++ myCodes_NodeJS/16-BlogApi_SessionCookíes/src/controllers/authController.js
@@ -4,33 +4,32 @@
 ------------------------------------------------------- */
 // Call Model;
 
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
 module.exports = {
   login: async (req, res) => {
-
-    const {email,password} = req.body
+    const { email, password } = req.body;
 
     //* email ve password'ü gönderip göndermedigini kontrol etmek icin:
 
     if (email && password) {
+      const user = await User.findOne({ email, password }); // findOne runs set method as a defult
 
-        const user = await User.findOne({email})
+      //* Gönderilen Email ve Password DB ile eslesiyor mu? Register islemi yapan kullanici icin Login islemi basarilidir. Daha önce Register islemi yapmamis kullanici DB'de bulunamaz.
 
-        if(user) {
-            res.send({
-                error: false,
-              })
-        }
-
-        
-
+      if (user) {
+        res.send({
+          error: false,
+          message: "login OK",
+        });
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("User credentials are wrong.");
+      }
     } else {
-        res.errorStatusCode = 401
-        throw new Error('Email and Password required.')
+      res.errorStatusCode = 401;
+      throw new Error("Email and Password required.");
     }
-
-    
   },
 
   logout: async (req, res) => {
