5e9b8e51e5277929a733dc75d7009ced15044d85
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-08 12:36:32 +0100

    add package.json

--- myCodes_NodeJS/17-BlogApi_SearchSortPagination/src/models/userModel.js
+++ myCodes_NodeJS/17-BlogApi_SearchSortPagination/src/models/userModel.js
@@ -4,6 +4,7 @@
 ------------------------------------------------------- */
 
 const mongoose = require("mongoose");
+const passwordEncrypte = require("../helpers/passwordEncrypt");
 
 /* -------------------------------------------------------*
 
