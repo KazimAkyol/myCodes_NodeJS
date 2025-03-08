ece20716801bbf7a9eba962a48f2fc3ec6e35a9e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-08 13:47:43 +0100

    add dbConnection.js

--- myCodes_NodeJS/18-PersonnelApi/index.js
+++ myCodes_NodeJS/18-PersonnelApi/index.js
@@ -1,38 +1,35 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-/*
-    $ npm i express dotenv mongoose express-async-errors
-    $ npm i cookie-session
-    $ npm i jsonwebtoken
-*/
 
-const express = require('express')
-const app = express()
+// $ npm i express dotenv mongoose express-async-errors
+// $ npm i cookie-session
+
+const express = require("express");
+const app = express();
 
 require("dotenv").config();
 const PORT = process.env?.PORT || 8000;
 
 /* ------------------------------------------------------- */
 // Accept JSON:
-app.use(express.json())
+app.use(express.json());
 
 // AsyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
-
-
-
-
+//* DB Connection:
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* ------------------------------------------------------- */
 // ErrorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
