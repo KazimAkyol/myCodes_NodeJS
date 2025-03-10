fa6bb8c6fa5fda941b78deef851896ac06ea3344
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-10 17:00:35 +0100

    add personnel.model.js

--- myCodes_NodeJS/18-PersonnelApi/src/models/personnel.model.js
+++ myCodes_NodeJS/18-PersonnelApi/src/models/personnel.model.js
@@ -1,6 +1,36 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+
+const PersonnelSchema = new mongoose.Schema(
+  {
+    departmentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      // set: (password)=> passwordEncrypt(password)
+      set: passwordEncrypt,
+    },
+  },
+  {
+    collection: "personnels",
+    timestamps: true,
+  }
+);
