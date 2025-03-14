774192f8cf65f663c0520a6706562b7bcbc4149b
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-14 11:34:54 +0100

    add auth.controller.js

--- myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/controllers/auth.controller.js
+++ myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/controllers/auth.controller.js
@@ -10,6 +10,18 @@ module.exports = {
 
     login: async (req, res) => {
 
+        const {username, email, password} = req.body
+
+        if ((username || email) && password) {
+            res.status(200).send({
+                error:false,
+            })
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please enter username/email and password')
+        }
+
+        
     },
 
     logout: async (req, res) => {
