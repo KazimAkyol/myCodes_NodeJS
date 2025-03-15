013c033583f08e9e084b86d2d15e123fb76161c0
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-15 18:01:07 +0100

    add tokenKey

--- myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/middlewares/authentication.js
+++ myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/middlewares/authentication.js
@@ -4,10 +4,33 @@
 ------------------------------------------------------- */
 //* Authentication Control Middleware
 
-module.exports = async (req,res,next)=> {
+const Token = require("../models/token.model");
 
+module.exports = async (req, res, next) => {
+  req.user = null;
 
+  // Authorization: Token ...tokenKey...
+  // Authorization: ApiKey ...tokenKey...
+  // Authorization: Bearer ...tokenKey...
+  // Authorization: Auth ...tokenKey...
+  // Authorization: X-API-KEY ...tokenKey...
+  // Authorization: x-auth-token ...tokenKey...
 
-    console.log('authentication middleware')
-    next()
-}
+  // Get Token from Headers:
+  // console.log(req.headers.authorization.split(" ")[1]); //* console.log atip sirasiyla req.headers yazip Thunder'da yazilana ulasilir, oradan Authorization'da gönderilen value'ya yani Token'a ulasilir. split yazarak bir array olarak yakalanir ve [1] yazarak Token'a en yalin haliyle ulasilir. Aynisini kod blogu seklinde yazalim:
+
+  const auth = req.headers.authorization || null; // Token ...TokenKey...
+  const tokenKey = auth ? auth.split(" ") : null; // ['Token', ...TokenKey...]
+
+  if (tokenKey && tokenKey[0] == "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+      "userId"
+    );
+
+    if (tokenData) req.user = tokenData.userId;
+  }
+
+  console.log(req.user);
+
+  next();
+};
