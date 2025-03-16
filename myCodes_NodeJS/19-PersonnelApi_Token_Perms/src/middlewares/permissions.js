b1302e9e2ca746d3a4b574a2b4e7f857113c6050
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-16 17:15:36 +0100

    some updated

--- myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/middlewares/permissions.js
+++ myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/middlewares/permissions.js
@@ -9,12 +9,23 @@ module.exports = {
 
     isLogin: (req,res,next) => {
 
-        if(req.user) {
+        if(req.user && req.user.isActive) {
 
             next()
         } else {
             res.errorStatusCode = 403
             throw new Error('NoPermission: You must login.')
         }
-    }
+    },
+
+    isAdmin: (req,res,next) => {
+
+        if(req.user && req.user.isActive && req.user.isAdmin) {
+
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Admin.')
+        }
+    },
 }
--- myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/routes/personnel.router.js
+++ myCodes_NodeJS/19-PersonnelApi_Token_Perms/src/routes/personnel.router.js
@@ -1,19 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
-const personnel = require('../controllers/personnel.controller');
+const router = require("express").Router();
+const personnel = require("../controllers/personnel.controller");
+const { isLogin } = require("../middlewares/permissions");
 /* ------------------------------------------------------- */
 // URL: /personnels
 
-router.route('/').get(personnel.list).post(personnel.create);
+router.use(isLogin); //* her bir route'dan önce isLogin permission'i kullanmak icin kisayolda bu sekilde yazilabilir.
 
-router.route('/:id')
-    .get(personnel.read)
-    .put(personnel.update)
-    .patch(personnel.update)
-    .delete(personnel.delete);
+router.route("/").get(personnel.list).post(personnel.create);
+
+router
+  .route("/:id")
+  .get(personnel.read)
+  .put(personnel.update)
+  .patch(personnel.update)
+  .delete(personnel.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
