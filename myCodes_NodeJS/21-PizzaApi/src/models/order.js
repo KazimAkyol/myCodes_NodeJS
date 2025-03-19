2a519d80a6b67d929aab8d8876cc297601f92811
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-19 20:54:46 +0100

    add order.js/model-controller

--- myCodes_NodeJS/21-PizzaApi/src/models/order.js
+++ myCodes_NodeJS/21-PizzaApi/src/models/order.js
@@ -1,6 +1,57 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    pizzaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pizza",
+      required: true,
+    },
+
+    size: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: ["Small", "Medium", "Large", "XLarge"], //* enum: Kullaniciya sinirli sayida secenek verdigimiz durumlarda kullanilan bir yapidir.
+    },
+
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    totalPrice: {
+      type: Number,
+      default: function () {
+        // Create
+        returnthis.quantity * this.price; //* this keyword'ü ile icinde bulunan objenin degiskeninden bahsediyor.
+      },
+      transform: function () {
+        // Update
+        return this.quantity * this.price;
+      },
+    },
+  },
+  {
+    collection: "orders",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
