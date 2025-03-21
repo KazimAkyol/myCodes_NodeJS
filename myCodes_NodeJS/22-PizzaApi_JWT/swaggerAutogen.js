4ec7066106f3f89ace2f16c55966ec72cbecd85b
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-21 23:49:02 +0100

    updated swagger.Autogen.js

--- myCodes_NodeJS/22-PizzaApi_JWT/swaggerAutogen.js
+++ myCodes_NodeJS/22-PizzaApi_JWT/swaggerAutogen.js
@@ -1,51 +1,56 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
 	| FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-require('dotenv').config()
-const HOST = process.env?.HOST || '127.0.0.1'
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
 /* ------------------------------------------------------- */
-const swaggerAutogen = require('swagger-autogen')()
-const packageJson = require('./package.json')
+const swaggerAutogen = require("swagger-autogen")();
+const packageJson = require("./package.json");
 
 const document = {
-	info: {
-		version: packageJson.version,
-		title: packageJson.title,
-		description: packageJson.description,
-		termsOfService: "http://www.CourseName.com/#",
-		contact: { name: packageJson.author, email: "clarusway@gmail.com" },
-		license: { name: packageJson.license, },
-	},
-	host: `${HOST}:${PORT}`,
-	basePath: '/',
-	schemes: ['http', 'https'],
-	consumes: ["application/json"],
-	produces: ["application/json"],
-	securityDefinitions: {
-		Token: {
-			type: 'apiKey',
-			in: 'header',
-			name: 'Authorization',
-			description: 'Simple Token Authentication * Example: <b>Token ...tokenKey...</b>'
-		},
-		Bearer: {
-			type: 'apiKey',
-			in: 'header',
-			name: 'Authorization',
-			description: 'JWT Authentication * Example: <b>Bearer ...accessToken...</b>'
-		},
-	},
-	security: [{ Token: [] }, { Bearer: [] }],
-	definitions: {
-		// Models:
-		// "User": require('./src/models/user').schema.obj,
-	}
-}
+  info: {
+    version: packageJson.version,
+    title: packageJson.title,
+    description: packageJson.description,
+    termsOfService: "http://www.CourseName.com/#",
+    contact: { name: packageJson.author, email: "clarusway@gmail.com" },
+    license: { name: packageJson.license },
+  },
+  host: `${HOST}:${PORT}`,
+  basePath: "/",
+  schemes: ["http", "https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+  securityDefinitions: {
+    Token: {
+      type: "apiKey",
+      in: "header",
+      name: "Authorization",
+      description:
+        "Simple Token Authentication * Example: <b>Token ...tokenKey...</b>",
+    },
+    Bearer: {
+      type: "apiKey",
+      in: "header",
+      name: "Authorization",
+      description:
+        "JWT Authentication * Example: <b>Bearer ...accessToken...</b>",
+    },
+  },
+  security: [{ Token: [] }, { Bearer: [] }],
+  definitions: {
+    // Order: require("./src/models/order"),
+    // Pizza: require("./src/models/pizza"),
+    // Token: require("./src/models/token"),
+    // Topping: require("./src/models/topping"),
+    // User: require("./src/models/user"),
+  },
+};
 
-const routes = ['./index.js']
-const outputFile = './src/configs/swagger.json'
+const routes = ["./index.js"];
+const outputFile = "./src/configs/swagger.json";
 
 // Create JSON file:
-swaggerAutogen(outputFile, routes, document)
+swaggerAutogen(outputFile, routes, document);
