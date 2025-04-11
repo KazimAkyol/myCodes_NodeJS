17d9a02056d2b2b68770e6977b80bd85ac11a402
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-12 00:27:26 +0200

    add todos ejs

--- myCodes_NodeJS/projects/Todo_Template/controllers/todo.controller.view.js
+++ myCodes_NodeJS/projects/Todo_Template/controllers/todo.controller.view.js
@@ -4,68 +4,68 @@
 ------------------------------------------------------- */
 // Controllers;
 
-const Todo = require('../models/todo.model');
+const Todo = require("../models/todo.model");
+
+//* Burada -1,0,1'in ne oldugunu ifade eden bir obje olusturduk ve tablodaki Priority'de bunu görüntüleyebilmek icin list'te res.render'da key-value'dan faydalanarak priorities key'inin value'su olarak atiyoruz.
 
 const PRIORITES = {
-    '-1': 'Low',
-    '0': "Normal",
-    '1': "High",
+  "-1": "Low",
+  0: "Normal",
+  1: "High",
 };
 
 module.exports = {
-    list: async (req, res) => {
+  list: async (req, res) => {
+    const result = await Todo.findAndCountAll();
 
-        const result = await Todo.findAndCountAll()
+    res.render("index", {
+      todos: result.rows, //* Burada result benim tüm todolarimi tutuyor
+      count: result.count,
+      priorities: PRIORITES,
+    });
+  },
 
-        res.render('index', { todos: result.rows, count: result.count, priorities: PRIORITES });
-    },
-
-    create: async (req, res) => {
-
-        if (req.method === 'POST') {
-            const result = await Todo.create(req.body);
-            res.redirect('/view')
-        } else {
-            res.render('todoCreate');
-        }
-    },
-
-    read: async (req, res) => {
-
-        // const result = await Todo.findOne({ where: { id: req.params.id } });
-        const result = await Todo.findByPk(req.params.id);
-
-        res.render('todoRead', { todo: result, priorities: PRIORITES });
-    },
-
-    update: async (req, res) => {
-
-        if (req.method === 'POST') {
-
-            await Todo.update(req.body, { where: { id: req.params.id } });
-
-            res.redirect('/view')
-
-        } else {
-
-            const result = await Todo.findByPk(req.params.id);
-
-            res.render('todoUpdate', { todo: result });
-        }
-
-    },
-
-    delete: async (req, res) => {
-
-        const result = await Todo.destroy({ where: { id: req.params.id } })
-
-        // todo when user click canel it still deletes the data. Fix please
-
-        if (result) {
-            res.redirect('/view')
-        } else { // failed
-            res.errorStatusCode = 404;
-            throw new Error('Delete is not succesful. Data is not found or already deleted.');
-        }
+  create: async (req, res) => {
+    if (req.method === "POST") {
+      const result = await Todo.create(req.body);
+      res.redirect("/view");
+    } else {
+      res.render("todoCreate");
     }
-}
+  },
+
+  read: async (req, res) => {
+    // const result = await Todo.findOne({ where: { id: req.params.id } });
+    const result = await Todo.findByPk(req.params.id);
+
+    res.render("todoRead", { todo: result, priorities: PRIORITES });
+  },
+
+  update: async (req, res) => {
+    if (req.method === "POST") {
+      await Todo.update(req.body, { where: { id: req.params.id } });
+
+      res.redirect("/view");
+    } else {
+      const result = await Todo.findByPk(req.params.id);
+
+      res.render("todoUpdate", { todo: result });
+    }
+  },
+
+  delete: async (req, res) => {
+    const result = await Todo.destroy({ where: { id: req.params.id } });
+
+    // todo when user click canel it still deletes the data. Fix please
+
+    if (result) {
+      res.redirect("/view");
+    } else {
+      // failed
+      res.errorStatusCode = 404;
+      throw new Error(
+        "Delete is not succesful. Data is not found or already deleted."
+      );
+    }
+  },
+};
--- myCodes_NodeJS/projects/Todo_Template/middlewares/errorHandler.js
+++ myCodes_NodeJS/projects/Todo_Template/middlewares/errorHandler.js
@@ -4,25 +4,24 @@
 ------------------------------------------------------- */
 
 module.exports = (err, req, res, next) => {
-    
-    const errorStatusCode = res.errorStatusCode ?? 500
+  const errorStatusCode = res.errorStatusCode ?? 500;
 
-    console.log('errorHandler worked.')
+  console.log("errorHandler worked.");
 
-    // console.log(req.originalUrl)
+  // console.log(req.originalUrl)
 
-    const data = {
-        error: true, // special data
-        message: err.message, // error string message
-        cause: err.cause, // error option cause
-        // stack: err.stack, // error details
-    }
+  const data = {
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  };
 
-    if (req.originalUrl.startsWith('/api')) {
+  if (req.originalUrl.startsWith("/api")) {
     // API -> JSON:
-        res.status(errorStatusCode).send(data)
-    } else {
+    res.status(errorStatusCode).send(data);
+  } else {
     // VIEW -> HTML
-        res.render('errors', { data })
-    }
-}
+    res.render("errors", { data });
+  }
+};
