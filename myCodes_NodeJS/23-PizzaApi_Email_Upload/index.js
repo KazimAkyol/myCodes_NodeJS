a3b080a3466120c0fb05c8a948830a15a246924d
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-04-19 21:52:06 +0200

    add sendMail.js

--- myCodes_NodeJS/23-PizzaApi_Email_Upload/index.js
+++ myCodes_NodeJS/23-PizzaApi_Email_Upload/index.js
@@ -39,14 +39,108 @@ app.use(require("./src/middlewares/queryHandler"));
 // Logger:
 app.use(require("./src/middlewares/logger"));
 
-/* ------------------------------------------------------- */
-// E-MAIL
+/* ------------------------------------------------------- *
+//* E-MAIL
 // $ npm i nodemailer
 // https://www.nodemailer.com/
 
 const nodemailer = require("nodemailer");
-/* ------------------------------------------------------- */
+
+/* ------- Send email with ethereal(fake) email ----------------- *
 //* Create Test Account:
+// nodemailer.createTestAccount().then((email) => console.log(email));
+//* Bu kod blogunu yazip fake bir email adresi olusturulur ve daha sonra sürekli fake email adresi olusturmasin diye bu kisim yoruma alinir.
+
+{
+  user: 'og4mqbpd3nww5t2j@ethereal.email',
+  pass: '8AcAnfaNcBT1GeyNxs',
+  smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+  imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+  pop3: { host: 'pop3.ethereal.email', port: 995, secure: true }, //* güncel olarak pop3 server'lari kullaniliyor.
+  web: 'https://ethereal.email',
+  mxEnabled: false
+}
+
+//* Connect to MailServer / SMTP
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "og4mqbpd3nww5t2j@ethereal.email",
+    pass: "8AcAnfaNcBT1GeyNxs",
+  },
+});
+
+// console.log(transporter);
+
+//* Send Mail:
+transporter.sendMail(
+  {
+    from: "og4mqbpd3nww5t2j@ethereal.email",
+    to: "info@clarusway.com",
+    subject: "Hello Clarusway",
+    text: "Hello there, how are your today?",
+    html: "<p>Hello there, how are your <b>today?<b> </p>/",
+  },
+  function (error, success) {
+    success ? console.log("SUCCESS:", success) : console.log("ERROR:", error);
+  }
+);
+
+/* - Send email with ethereal(fake) email - */
+
+/* - Send email with gmail service - *
+//* GoogleMail (gmail)
+// Google -> AccountHome -> Security -> Two-Step-Verify (make it on) -> App-Passwords (if not showing use this link: https://myaccount.google.com/apppasswords)
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "akyolkzm.2016493@gmail.com",
+    pass: "fvng wwem jrmn lvim", //* gmail tarafindan bize verilen password
+  },
+});
+
+transporter.sendMail(
+  {
+    from: "akyolkzm.2016493@gmail.com",
+    to: "info@clarusway.com",
+    subject: "Hello Clarusway",
+    text: "Hello there, how are your today?",
+    html: "<p>Hello there, how are your <b>today?<b> </p>/",
+  },
+  function (error, success) {
+    success ? console.log("SUCCESS:", success) : console.log("ERROR:", error);
+  }
+);
+
+/* - Send email with gmail service - */
+
+/* - Send email with yandex service - *
+//* Yandex mail
+
+const transporter = nodemailer.createTransport({
+  service: "yandex",
+  auth: {
+    user: "test@yandex.com",
+    pass: "yourPersonelPassword", //* yandex tarafindan bize verilen password
+  },
+});
+
+transporter.sendMail(
+  {
+    from: "og4mqbpd3nww5t2j@ethereal.email",
+    to: "info@clarusway.com",
+    subject: "Hello Clarusway",
+    text: "Hello there, how are your today?",
+    html: "<p>Hello there, how are your <b>today?<b> </p>/",
+  },
+  function (error, success) {
+    success ? console.log("SUCCESS:", success) : console.log("ERROR:", error);
+  }
+);
+/* - Send email with yandex service - */
 
 /* ------------------------------------------------------- */
 //* Routes:
---
+++ myCodes_NodeJS/23-PizzaApi_Email_Upload/src/helpers/sendMail.js
@@ -0,0 +1,30 @@
+"use strict";
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+
+const nodemailer = require("nodemailer");
+
+module.exports = function sendMail(to, subject, message) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "akyolkzm.2016493@gmail.com",
+      pass: "fvng wwem jrmn lvim", //* gmail tarafindan bize verilen password
+    },
+  });
+
+  // Send Mail
+  transporter.sendMail(
+    {
+      from: "akyolkzm.2016493@gmail.com",
+      to: to,
+      subject: subject,
+      text: message,
+      html: message,
+    },
+    function (error, success) {
+      success ? console.log("SUCCESS:", success) : console.log("ERROR:", error);
+    }
+  );
+};
